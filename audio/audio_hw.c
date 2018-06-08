LineageOS/android_device_samsung_i9300

i9300: audio: fix bt sample rate handling
Use the bluetooth stack for narrow/wideband rate detection.

Based on a commit for the common samsung HAL by
Christopher N. Hesse <raymanfx@gmail.com>

Change-Id: Ibeb2bde0318922cd2dbc0efac5511dfd676137b3

audio/audio_hw.c
@@ -104,6 +104,7 @@ struct m0_audio_device {
     bool mic_mute;
     struct echo_reference_itfe *echo_reference;
     bool bluetooth_nrec;
+    bool bluetooth_wb;
     int wb_amr;
     bool screen_off;
 
@@ -362,7 +363,7 @@ static int start_call(struct m0_audio_device *adev)
 
     if (bt_on) {
        /* use amr-nb for bluetooth */
-       pcm_config_vx.rate = VX_NB_SAMPLING_RATE;
+       pcm_config_vx.rate = adev->bt_wideband ? VX_WB_SAMPLING_RATE : VX_NB_SAMPLING_RATE;
     } else {
        pcm_config_vx.rate = adev->wb_amr ? VX_WB_SAMPLING_RATE : VX_NB_SAMPLING_RATE;
     }
@@ -2534,6 +2535,16 @@ static void adev_close_output_stream(struct audio_hw_device *dev,
     free(stream);
 }
 
+static void adev_set_voice_session_bt_wideband(struct m0_audio_device *adev, bool enable)
+{
+    adev->bluetooth_wb = enable;
+
+    if (adev->mode == AUDIO_MODE_IN_CALL) {
+        stop_call(adev);
+        start_call(adev);
+    }
+}
+
 static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
 {
     struct m0_audio_device *adev = (struct m0_audio_device *)dev;
@@ -2549,6 +2560,14 @@ static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
             adev->bluetooth_nrec = true;
         else
             adev->bluetooth_nrec = false;
+    }
+
+    ret = str_parms_get_str(parms, AUDIO_PARAMETER_KEY_BT_SCO_WB, value, sizeof(value));
+    if (ret >= 0) {
+        if (strcmp(value, AUDIO_PARAMETER_VALUE_ON) == 0)
+            adev_set_voice_session_bt_wideband(adev, true);
+        else
+            adev_set_voice_session_bt_wideband(adev, false);
     }
 
     ret = str_parms_get_str(parms, "screen_off", value, sizeof(value));

encaro/RR7_android_device_samsung_i9300

audio/audio_hw.c
@@ -104,6 +104,7 @@ struct m0_audio_device {
     bool mic_mute;
     struct echo_reference_itfe *echo_reference;
     bool bluetooth_nrec;
+    bool bluetooth_wb;
     int wb_amr;
     bool screen_off;
 
@@ -362,7 +363,7 @@ static int start_call(struct m0_audio_device *adev)
 
     if (bt_on) {
        /* use amr-nb for bluetooth */
-       pcm_config_vx.rate = VX_NB_SAMPLING_RATE;
+       pcm_config_vx.rate = adev->bluetooth_wb ? VX_WB_SAMPLING_RATE : VX_NB_SAMPLING_RATE;
     } else {
        pcm_config_vx.rate = adev->wb_amr ? VX_WB_SAMPLING_RATE : VX_NB_SAMPLING_RATE;
     }
@@ -2534,6 +2535,16 @@ static void adev_close_output_stream(struct audio_hw_device *dev,
     free(stream);
 }
 
+static void adev_set_voice_session_bt_wideband(struct m0_audio_device *adev, bool enable)
+{
+    adev->bluetooth_wb = enable;
+
+    if (adev->mode == AUDIO_MODE_IN_CALL) {
+        end_call(adev);
+        start_call(adev);
+    }
+}
+
 static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
 {
     struct m0_audio_device *adev = (struct m0_audio_device *)dev;
@@ -2549,6 +2560,14 @@ static int adev_set_parameters(struct audio_hw_device *dev, const char *kvpairs)
             adev->bluetooth_nrec = true;
         else
             adev->bluetooth_nrec = false;
+    }
+
+    ret = str_parms_get_str(parms, AUDIO_PARAMETER_KEY_BT_SCO_WB, value, sizeof(value));
+    if (ret >= 0) {
+        if (strcmp(value, AUDIO_PARAMETER_VALUE_ON) == 0)
+            adev_set_voice_session_bt_wideband(adev, true);
+        else
+            adev_set_voice_session_bt_wideband(adev, false);
     }
 
     ret = str_parms_get_str(parms, "screen_off", value, sizeof(value));